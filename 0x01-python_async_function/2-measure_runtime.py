ee21695dd28e0656897a15b98d0819d568e35e53
sarora2200 <sarora2200@gmail.com>
2024-04-22 16:31:05 +0000

    done

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""task2"""
+import asyncio
+import time
+
+
+wait_n = __import__('2-measure_runtime.py').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """measure an approximate elapsed time"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - start_time) / n
